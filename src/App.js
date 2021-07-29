d9327ac16080386507c90230abeb72e1f24bf417
msumari <suleykhery@gmail.com>
2021-07-29 13:18:16 +0300

    test tailwind

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-red-500">lume</h1>
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="bg-gray-900">
+      <h1 className="text-red-700">homepage</h1>
+    </div>
+  );
+};
+
+export default Home;
