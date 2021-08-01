f5d2e2af122e02845d7ae60a1d9aa48397d5b410
Hatibu <juliushatibu@gmail.com>
2021-08-01 15:48:29 +0300

    Add contents on navigtion bar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,10 @@
+import React from "react";
+import Navbar from '../components/Navbar'
 const Home = () => {
   return (
-    <div className="bg-gray-900">
-      <h1 className="text-red-700">Our homepage</h1>
+    <div className="">
+      <Navbar />
+      
     </div>
   );
 };
