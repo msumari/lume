9e1deb234ea3189a7bfe13ace05a50101ab705a4
msumari <suleykhery@gmail.com>
2021-08-05 11:13:39 +0300

    login page setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import "./App.css";
 
-import Home from "./components/Home";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
-     
-      <Home />
+      <Login />
     </div>
   );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+function Login() {
+  return (
+    <div className="w-full h-screen bg-loginbanner bg-cover bg-center">
+      <div className="w-full flex justify-between">
+        <img src="./images/netflix-logo.png" alt="logo" className="h-24" />
+      </div>
+      <div className="grid place-items-center w-full mt-60"></div>
+    </div>
+  );
+}
+
+export default Login;
