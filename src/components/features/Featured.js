aceea97db99816edf2220adde7996b65a7db5e69
Hatibu <juliushatibu@gmail.com>
2021-08-07 23:53:14 +0300

    Minor changes on banner section including styling buttons

--- src/components/features/Featured.js
+++ src/components/features/Featured.js
@@ -4,26 +4,25 @@ import {BsInfoCircle} from 'react-icons/bs'
 
 const Featured = () => {
     return (
-        <div className="bg-bannerWallpaper bg-center bg-cover h-screen">
-            <div className="featured  "> 
-          <div className="info lg:w-4/12 lg:absolute lg:top-full">
+        <div className="-mt-20 bg-bannerWallpaper bg-center bg-cover h-screen">
+            <div className="info lg:w-4/12  lg:ml-20 lg:mt-20">
           <img
           src="https://occ-0-1432-1433.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABUZdeG1DrMstq-YKHZ-dA-cx2uQN_YbCYx7RABDk0y7F8ZK6nzgCz4bp5qJVgMizPbVpIvXrd4xMBQAuNe0xmuW2WjoeGMDn1cFO.webp?r=df1"
           alt=""
         />
-        <span className="desc">
+        <span className="desc text-white">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
           adipisci repellendus eum quasi illo, velit numquam, maxime tempora
           sint deleniti, aliquid qui? Facilis, adipisci! Ratione hic repudiandae
           temporibus eum earum?
         </span>
 
-        <div className="buttons">
-          <button className="play">
+        <div className="buttons lg:flex lg:mt-4">
+          <button className="play bg-white flex items-center lg:px-4 lg:py-2 lg:mr-3 rounded">
             <FaCaretRight />
-            <span></span>
+            <span>Play</span>
           </button>
-          <button className="moreInfo">
+          <button className="moreInfo bg-gray-400 flex items-center lg:px-4 lg:py-2 rounded">
             <BsInfoCircle />
             <span>Info</span>
           </button>
@@ -31,8 +30,6 @@ const Featured = () => {
         </div>
 
           </div>
-
-            </div>
         </div>
     )
 }
