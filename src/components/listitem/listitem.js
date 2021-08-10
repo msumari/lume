e3cd6a033f99b193bf8c178dfcd1a278a32823bd
Hatibu <juliushatibu@gmail.com>
2021-08-10 23:24:57 +0300

    feat: functionality for movies slider

--- src/components/listitem/listitem.js
+++ src/components/listitem/listitem.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const listitem = () => {
     return (
-        <div className=" h-28 bg-white mr-3 w-full">
+        <div className=" h-28 bg-white mr-3 px-20">
             item contents
         </div>
     )
