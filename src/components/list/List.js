5418084cc660799209ab5e7daf855dd68c747cf6
msumari <suleykhery@gmail.com>
2021-10-07 11:23:34 +0300

    feat: Added Scrollbar Styling

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -1,45 +1,38 @@
 import ListItem from "../listitem/listitem";
-import React, { useRef, useState } from "react";
-import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
+import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 const List = ({ list }) => {
-
   const responsive = {
     superLargeDesktop: {
       // the naming can be any, depends on you.
       breakpoint: { max: 4000, min: 3000 },
-      items: 7
+      items: 7,
     },
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 5
+      items: 5,
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
-      items: 2
+      items: 2,
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
-      items: 1
-    }
+      items: 1,
+    },
   };
 
- 
   return (
     <div className="list bg-black pt-10  ">
       <span className="listTitle text-white text-xl ml-14">{list.title}</span>
       <div className="wrapper ml-12 relative">
-       
-       <Carousel responsive={responsive}>
-      
+        <Carousel responsive={responsive}>
           {list.content.map((item, i) => (
             <ListItem index={i} item={item} />
           ))}
-      
         </Carousel>
-       
       </div>
     </div>
   );
