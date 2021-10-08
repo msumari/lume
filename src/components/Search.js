2b05dd7f7ac90b64eaac26b3d0c9b2b0e04132e0
msumari <suleykhery@gmail.com>
2021-10-08 16:57:13 +0300

    feat:Added search recommendation

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 
 function Search() {
+  const [term, setTerm] = useState("");
   const [title, setTitle] = useState();
 
   useEffect(() => {
@@ -25,6 +26,7 @@ function Search() {
     getTitles();
   }, []);
 
+  console.log(term);
   console.log(title);
 
   return (
@@ -34,12 +36,44 @@ function Search() {
       </Link>
       <form className="flex w-full items-center justify-center -mt-80">
         <input
-          type="term"
+          type="text"
           placeholder="Search"
           className="w-1/2 h-12 rounded-lg px-4 border-white border-2 bg-black opacity-70 placeholder-white hover:border-red-700 cursor-pointer hover:bg-white focus:border-red-500 focus:outline-none focus:bg-white"
+          onChange={(e) => {
+            setTerm(e.target.value);
+          }}
         />
         <FaSearch className="text-white text-2xl -ml-10 z-10 hover:text-red-500 cursor-pointer animate-pulse" />
       </form>
+      <div className="-mt-96  h-48 overflow-auto w-1/2 rounded-lg ml-4">
+        {title &&
+          title
+            .filter((name) => {
+              if (term === "") {
+                return (
+                  <div>
+                    <p>No relevant video avilable</p>
+                  </div>
+                );
+              } else if (
+                name.title.toLowerCase().includes(term.toLowerCase())
+              ) {
+                return name;
+              }
+            })
+            .map((name) => {
+              return (
+                <div
+                  key={name.id}
+                  className="border-white border-b-2 cursor-pointer hover:bg-white"
+                >
+                  <h1 className="text-white hover:text-red-700 p-2 text-2xl font-bold ">
+                    {name.title}
+                  </h1>
+                </div>
+              );
+            })}
+      </div>
     </div>
   );
 }
