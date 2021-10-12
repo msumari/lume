813907930cbd2b1e8557518023f5892bde242090
msumari <suleykhery@gmail.com>
2021-10-12 13:28:30 +0300

    fix:git merge conflict

--- src/components/listitem/Listitem.js
+++ src/components/listitem/Listitem.js
@@ -4,7 +4,7 @@ import { FcLike } from "react-icons/fc";
 import { MdPlayArrow, MdPlaylistAdd } from "react-icons/md";
 import { AiFillDislike } from "react-icons/ai";
 import { Link } from "react-router-dom";
-import '../../App.css'
+import "../../App.css";
 
 const Listitem = ({ index, item }) => {
   const [isHovered, setIsHovered] = useState(false);
@@ -13,8 +13,6 @@ const Listitem = ({ index, item }) => {
   useEffect(() => {
     const getMovie = async () => {
       try {
-
-
         const res = await axios.get("/api/movie/find/" + item);
         setMovies(res.data);
       } catch (err) {
@@ -38,27 +36,25 @@ const Listitem = ({ index, item }) => {
             <span className="absolute px-8 uppercase ">{movies.title}</span>
             {/* <span>{movies.genre}</span> */}
           </div>
-         
+
           <div className="">
-             <video
-            src={movies.trailer}
-            autoPlay={true}
-            muted={true}
-            loop
-            className="video -mt-44 w-64 h-80  "
-           
-          />
+            <video
+              src={movies.trailer}
+              autoPlay={true}
+              muted={true}
+              loop
+              className="video -mt-44 w-64 h-80  "
+            />
             <div className="flex gap-8 items-center absolute -mt-28">
-              <Link to={{ pathname: "/watch", movie: movies }} >
+              <Link to={{ pathname: "/watch", movie: movies }}>
                 <MdPlayArrow color="white" size={20} />
               </Link>
               <MdPlaylistAdd color="white" />
               <FcLike />
               <AiFillDislike color="white" />
               <span className="text-white">+{movies.limit}</span>
-               {/* <span className="">{movies.year}</span> */}
+              {/* <span className="">{movies.year}</span> */}
             </div>
-           
           </div>
         </div>
       )}
