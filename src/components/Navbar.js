ee26dc02fdbd3205022214ae26b504413a98fe1c
msumari <suleykhery@gmail.com>
2021-10-13 18:57:35 +0300

    fix:resolved the class element dom warning

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import { BsBell, BsCaretDownFill } from "react-icons/bs";
-import { MdMoreVert} from "react-icons/md";
-import { VscClose } from "react-icons/vsc";
+import { MdMoreVert } from "react-icons/md";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 
@@ -28,39 +27,48 @@ const Navbar = () => {
         <div className="text-white md:text-md lg:text-md ">
           <div className="left lg:flex lg:items-center item-center">
             <div className="py-3 px-5 flex items-center justify-between">
-            <Link to="/">
-              <img className="h-6 lg:h-10" src="/images/lume.png" alt="lume" />
-            </Link>
+              <Link to="/">
+                <img
+                  className="h-6 lg:h-10"
+                  src="/images/lume.png"
+                  alt="lume"
+                />
+              </Link>
 
-            <Link to="/search">
-            <FaSearch className="lg:hidden visible lg:ml-2" />
-          </Link>
-          <img
-            className=" lg:hidden h-4 lg:h-5 lg:w-5 lg:ml-2 lg:rounded-sm"
-            src="/images/profile.png"
-            alt=""
-          />
-          <div class="dropdown lg:hidden visible inline-block relative">
-            <BsCaretDownFill className="lg:ml-2" />
-            <ul class="dropdown-menu absolute hidden text-gray-700 pt-1">
-              <li class="">
-                <a class="bg-black text-white hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap">
-                  Logout
-                </a>
-              </li>
-            </ul>
-          </div>
-               {/* Humberger for mobile view */}
-           <div className="block lg:hidden">
-            <div onClick={handleClick} className=" ">
-               { click ? <MdMoreVert  /> : <MdMoreVert />}
+              <Link to="/search">
+                <FaSearch className="lg:hidden visible lg:ml-2" />
+              </Link>
+              <img
+                className=" lg:hidden h-4 lg:h-5 lg:w-5 lg:ml-2 lg:rounded-sm"
+                src="/images/profile.png"
+                alt=""
+              />
+              <div className="dropdown lg:hidden visible inline-block relative">
+                <BsCaretDownFill className="lg:ml-2" />
+                <ul className="dropdown-menu absolute hidden text-gray-700 pt-1">
+                  <li className="">
+                    <a className="bg-black text-white hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap">
+                      Logout
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              {/* Humberger for mobile view */}
+              <div className="block lg:hidden">
+                <div onClick={handleClick} className=" ">
+                  {click ? <MdMoreVert /> : <MdMoreVert />}
+                </div>
+              </div>
+              {/* End */}
             </div>
-           </div>
-                 {/* End */}
 
-            </div>
-            
-            <div className={click ? "px-5 py-3.5  grid lg:flex content-center lg:ml-8 " : "px-5  grid lg:flex content-center lg:ml-8 hidden"}>
+            <div
+              className={
+                click
+                  ? "px-5 py-3.5  grid lg:flex content-center lg:ml-8 "
+                  : "px-5  lg:flex content-center lg:ml-8 hidden"
+              }
+            >
               <Link to="/">
                 <span className="lg:ml-4">Home</span>
               </Link>
@@ -82,24 +90,23 @@ const Navbar = () => {
           </Link>
 
           <BsBell className=" hidden lg:inline-block lg:ml-2" />
-         
+
           <img
             className=" hidden lg:inline-block h-4 lg:h-5 lg:w-5 lg:ml-2 lg:rounded-sm"
             src="/images/profile.png"
             alt=""
           />
 
-          <div class="dropdown hidden  lg:inline-block relative">
+          <div className="dropdown hidden  lg:inline-block relative">
             <BsCaretDownFill className="lg:ml-2" />
-            <ul class="dropdown-menu absolute hidden text-gray-700 pt-1">
-              <li class="">
-                <a class="bg-black text-white hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap">
+            <ul className="dropdown-menu absolute hidden text-gray-700 pt-1">
+              <li className="">
+                <a className="bg-black text-white hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap">
                   Logout
                 </a>
               </li>
             </ul>
           </div>
-
         </div>
       </header>
     </div>
