d2f87a5ca8fab82cdb2280df2ba87b9801573cfe
Hatibu <juliushatibu@gmail.com>
2021-12-12 17:16:32 +0300

    style:removed break point

--- src/components/Result.js
+++ src/components/Result.js
@@ -3,25 +3,7 @@ import { FaWindowClose } from "react-icons/fa";
 import Searchlist from "./listitem/Searchlist";
 
 function Result({ data, term, setShow }) {
-  const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 7,
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 5,
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2,
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
-    },
-  };
+
 
   const modalClose = () => {
     setShow(false);
